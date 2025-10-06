63313f3afda3efa3a907a5b2e45c05603a687502
Seoan <seoan102410@gmail.com>
2025-10-06 17:02:10 +0900

    Update suggestion.tsx

--- components/suggestion.tsx
+++ components/suggestion.tsx
@@ -51,7 +51,7 @@ export const Suggestion = ({
           <div className="flex flex-row items-center justify-between">
             <div className="flex flex-row items-center gap-2">
               <div className="size-4 bg-muted-foreground/25 rounded-full" />
-              <div className="font-medium">Assistant</div>
+              <div className="font-medium">어시스턴트</div>
             </div>
             <button
               type="button"
@@ -69,7 +69,7 @@ export const Suggestion = ({
             className="w-fit py-1.5 px-3 rounded-full"
             onClick={onApply}
           >
-            Apply
+            적용 {/* Apply -> 적용 */}
           </Button>
         </motion.div>
       )}
